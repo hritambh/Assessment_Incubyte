46de044cb7692766369a0ff01186305393002f37
hritambh <hritambh9@gmail.com>
2024-10-11 13:09:59 +0530

    Refactoring add method to handle output for two nos

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,6 +5,11 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(numbers);  // Handle single number case
+        String[] nums = numbers.split(",");
+        if (nums.length == 1) {
+            return Integer.parseInt(nums[0]);
+        } else {
+            return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+        }
     }
 }
