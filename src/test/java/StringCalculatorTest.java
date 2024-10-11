d279bc34031c728dd301dbcfa39a2c9806a90963
hritambh <hritambh9@gmail.com>
2024-10-11 15:45:13 +0530

    Adding failing test for multiple numbers input

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -26,4 +26,11 @@ public class StringCalculatorTest {
         int result = calculator.add("1,2");
         assertEquals(3, result);  // The input "1,2" should return 3
     }
+
+    @Test
+    public void testAddMultipleNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1,2,3");
+        assertEquals(6, result);  // The input "1,2,3" should return 6
+    }
 }
